a314646e29805c88731f3147160b72ad66414b3a
Tarcisio Coutinho <tcs5cin@gmail.com>
2022-12-08 21:42:04 -0300

    basic auth

---
+++ src/main/java/io/tacsio/security/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package io.tacsio.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityConfig(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+}
---
+++ src/test/java/io/tacsio/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tacsio.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
