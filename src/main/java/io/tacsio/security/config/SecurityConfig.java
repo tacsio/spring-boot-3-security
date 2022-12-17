831180161b709c898be24084aa5842505b7deb31
Tarcisio Coutinho <tcs5cin@gmail.com>
2022-12-17 20:55:15 -0300

    user details service

--- src/main/java/io/tacsio/security/config/SecurityConfig.java
+++ src/main/java/io/tacsio/security/config/SecurityConfig.java
@@ -4,11 +4,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    private final static List<UserDetails> APPLICATION_USERS = Arrays.asList(
+            new User("tacsio@mail.com", "password", Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))),
+            new User("user@mail.com", "password", Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")))
+    );
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
 
     @Bean
     public SecurityFilterChain defaultSecurityConfig(HttpSecurity http) throws Exception {
@@ -17,8 +38,17 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return email -> APPLICATION_USERS.stream()
+                .filter(u -> u.getUsername().equals(email))
+                .findFirst()
+                .orElseThrow(() -> new UsernameNotFoundException("No user was found."));
     }
 }
